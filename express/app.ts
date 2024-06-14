8c0a4af7d7a5bc3bd0085df948963093d38b661f
youyoumu <19760884+youyoumu@users.noreply.github.com>
2024-06-15 00:49:02 +0700

    Create api for user sign up

--- express/app.ts
+++ express/app.ts
@@ -4,16 +4,74 @@ import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 const app: Express = express()
 const port = 3000
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!')
 })
 
-app.get('/users', async (res: Response, req: Request) => {
+app.get('/users', async (req: Request, res: Response) => {
   const users = await prisma.user.findMany()
   res.send(users)
+})
+
+app.post('/users', async (req: Request, res: Response) => {
+  if (!req.body) {
+    return res.sendStatus(400)
+  }
+
+  const { username, password } = req.body.user
+    ? req.body.user
+    : { username: '', password: '' }
+  const user = await validateUserCreate(username, password)
+  if (user.error.length === 0) {
+    const user = await prisma.user.create({
+      data: {
+        username,
+        password
+      }
+    })
+    return res.json({ user })
+  } else {
+    return res.status(400).json({ user })
+  }
 })
 
 app.listen(port, () => {
   console.log(`listening on port ${port}`)
 })
+
+async function validateUserCreate(username: string, password: string) {
+  const error: string[] = []
+  const user = {
+    username,
+    password,
+    error
+  }
+
+  if (username === '') {
+    error.push('Username is required')
+  }
+  if (password === '') {
+    error.push('Password is required')
+  }
+  if (username.length < 3) {
+    error.push('Username must be at least 3 characters')
+  }
+  if (password.length < 3) {
+    error.push('Password must be at least 3 characters')
+  }
+
+  const userInDb = await prisma.user.findUnique({
+    where: {
+      username: username
+    }
+  })
+
+  if (userInDb) {
+    error.push('Username already exists')
+  }
+
+  return user
+}
