ddc3a826c0042719c4297d6e76fc1ffc2c28201a
youyoumu <19760884+youyoumu@users.noreply.github.com>
2024-06-14 17:01:52 +0700

    Use express with typecript

--- express/app.ts
+++ express/app.ts
@@ -1,15 +1,15 @@
-import express from 'express'
+import express, { Express, Request, Response } from 'express'
 import { PrismaClient } from '@prisma/client'
 
 const prisma = new PrismaClient()
-const app = express()
+const app: Express = express()
 const port = 3000
 
-app.get('/', (req, res) => {
+app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!')
 })
 
-app.get('/users', async (req, res) => {
+app.get('/users', async (res: Response, req: Request) => {
   const users = await prisma.user.findMany()
   res.send(users)
 })
