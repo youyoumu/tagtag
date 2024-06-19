928fd57ae1a771589bb7e5ec92d5594200b0224f
youyoumu <19760884+youyoumu@users.noreply.github.com>
2024-06-19 12:56:00 +0700

    Add discord user id to content model

---
+++ prisma/migrations/20240619055519_add_discord_user_id/migration.sql
@@ -0,0 +1,18 @@
+-- RedefineTables
+PRAGMA defer_foreign_keys=ON;
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Content" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "title" TEXT NOT NULL DEFAULT '',
+    "body" TEXT NOT NULL DEFAULT '',
+    "tags" TEXT NOT NULL DEFAULT '',
+    "user_id" INTEGER NOT NULL,
+    "discord_user_id" TEXT NOT NULL DEFAULT '',
+    CONSTRAINT "Content_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+INSERT INTO "new_Content" ("body", "created_at", "id", "tags", "title", "user_id") SELECT "body", "created_at", "id", "tags", "title", "user_id" FROM "Content";
+DROP TABLE "Content";
+ALTER TABLE "new_Content" RENAME TO "Content";
+PRAGMA foreign_keys=ON;
+PRAGMA defer_foreign_keys=OFF;
